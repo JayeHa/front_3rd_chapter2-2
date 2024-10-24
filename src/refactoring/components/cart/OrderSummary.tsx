b7f20ade8f9e615cfc4a04b22f844516861cfb27
JayeHa <jwha94@gmail.com>
2024-10-24 13:15:16 +0900

    refactor: CartPage 컴포넌트 분리

---
+++ src/refactoring/components/cart/OrderSummary.tsx
@@ -0,0 +1,20 @@
+import { useCart } from '../../hooks';
+
+type Props = Pick<ReturnType<typeof useCart>, 'calculateTotal'>;
+
+export const OrderSummary = ({ calculateTotal }: Props) => {
+  const { totalBeforeDiscount, totalAfterDiscount, totalDiscount } =
+    calculateTotal();
+
+  return (
+    <div className="space-y-1">
+      <p>상품 금액: {totalBeforeDiscount.toLocaleString()}원</p>
+      <p className="text-green-600">
+        할인 금액: {totalDiscount.toLocaleString()}원
+      </p>
+      <p className="text-xl font-bold">
+        최종 결제 금액: {totalAfterDiscount.toLocaleString()}원
+      </p>
+    </div>
+  );
+};
